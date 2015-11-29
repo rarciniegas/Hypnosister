3b633af0b5bcdf331b86250fc02f1c04868b2300
rarciniegas <rarci003@fiu.edu>
2015-11-29 17:37:01 -0500

    First push

---
+++ Hypnosister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnosister
+//
+//  Created by roberto arciniegas on 11/27/15.
+//  Copyright (c) 2015 roberto arciniegas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hypnosister/RAHypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  RAHypnosisView.h
+//  Hypnosister
+//
+//  Created by roberto arciniegas on 11/27/15.
+//  Copyright (c) 2015 roberto arciniegas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RAHypnosisView : UIView
+
+@end
